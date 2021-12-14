7a5d0d2d37090bf41031aa2dd234099c434941b8
Artorias-theMindful <76874344+Artorias-theMindful@users.noreply.github.com>
2021-12-14 19:29:41 +0200

    123

---
+++ Lab1/lab_1_task_1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace asd_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z, a, b;
+            Console.Write("x = "); x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y = "); y = Convert.ToDouble(Console.ReadLine());
+            Console.Write("z = "); z = Convert.ToDouble(Console.ReadLine());
+            if (x > 0 && x * z != -2)
+            {
+                a = Math.Tan(Math.Pow(x, -y) + z / (y * y + 1) + Math.Pow(y / (x * z + 2), 1.0 / 3.0));
+                Console.WriteLine("a = " + a.ToString());
+                if (z != 0 && Math.Sin((x + Math.PI * y) / z) != 0)
+                {
+                    b = a / Math.Sin((x + Math.PI * y) / z);
+                    Console.WriteLine("b = " + b.ToString());
+                }
+                else
+                    Console.WriteLine("b does not exist");
+            }
+            else
+                Console.WriteLine("a and b do not exist");
+            
+
+
+
+        }
+    }
+}
---
+++ Lab1/lab_1_task_2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace lab_1_task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            try
+            {
+
+                Console.Write("y = "); string y1 = Console.ReadLine();
+                if (Convert.ToInt32(y1) >= 1582 && Convert.ToInt32(y1) <= 4902)
+                {
+                    int d, c, y;
+                    c = Convert.ToInt32(y1[0].ToString() + y1[1].ToString());
+                    y = Convert.ToInt32(y1[2].ToString() + y1[3].ToString());
+                    d = 25;
+                    while (true)
+                    {
+                        if ((d + Math.Truncate(2.6 * 8.0 - 0.2) + y + Math.Truncate(y / 4.0) + Math.Truncate(c / 4.0) - 2 * c) % 7 == 0)
+                        {
+                            break;
+                        }
+                        else
+                            d++;
+                    }
+                    Console.WriteLine($"The day is {d}");
+                }
+                else
+                    Console.WriteLine("wrong number");
+            }
+            catch
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
